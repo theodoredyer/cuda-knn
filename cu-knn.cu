8c166acfb5fca723472fe0b96dd66f3744de381f
theodoredyer <tdyer@ucsc.edu>
2022-04-21 16:12:23 -0700

    Update cu-knn.cu

--- cu-knn.cu
+++ cu-knn.cu
@@ -3,26 +3,44 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <sstream>
+
 using namespace std;
 
 
 int main() {
 
+    double dataframe[918][12];
+
     // Opening our data file
     string line;
+    string element;
     ifstream inputfile("datasets/clean_data.csv");
 
-
     // Processing input file line by line
-    int tracker = 0;
+    int iterations = 0;
+
+    // Start file stream
     if(inputfile.is_open()) {
+        
+        // While there is a line to read (should happen 917 times)
         while(getline(inputfile,line) && tracker < 1) {
-            tracker += 1;
-            cout << line << '\n';
+
+            // cout << line << endl;
+
+            // Tokenize the line
+            stringstream X(line);
+            while(getline(X, element, ',')) {
+                cout << element << endl;
+            }
+
+            iterations += 1;
         }
+
+        // Done Reading 
         inputfile.close();
     } else {
-        cout << "unable to open file";
+        cout << "unable to open data file";
     }
 
     return 0;
